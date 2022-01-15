bbebe3763fc7582e41b1a52ba374f59abaac89e4
Regina Grogan <regina.r.grogan@gmail.com>
2022-01-14 18:11:46 -0600

    Implemented functions that load questions.csv into database

--- database-mongo/models/question.js
+++ database-mongo/models/question.js
@@ -1,4 +1,4 @@
-const db = require('../database-mongo');
+const db = require('../index.js');
 const fs = require('fs');
 const path = require('path');
 const csv = require('csv-parser');
@@ -12,46 +12,66 @@ const questionSchema = new db.Schema({
   asker_email: {type: String, required: true},
   reported: {type: Boolean, default: false},
   question_helpfulness: {type: Number, required: true},
-  answers: [{type: db.Schema.Types.ObjectId, ref: 'Answer'}]
+  answers: [{
+          answer_id: Number,
+          question_id: Number,
+          body: String,
+          date: {type: Date, default: Date.now},
+          answerer_name: String,
+          helpfulness: Number,
+          reported: {type: Boolean, default: false},
+          photos: [{id: Number, url: String}]
+  }]
 });
 
 const Question = db.model("Question", questionSchema);
 
-// let addPreviousData = () => {
-//   let filename = path.join(__dirname, 'questions.csv')
-//   let dataToInsert = [];
-//   fs.createReadStream(filename)
-//   .pipe(csv(['question_id', 'product_id', 'question_body', 'question_date', 'asker_name', 'asker_email', 'reported', 'question_helpfulness']))
-//   .on('data', (data) => Question.create(data))
-//   .on('end', () => {
-//     console.log('Ended');
-//   });
-  // let data = fs.readFileSync(filename, (err, fileData) {
-  //   parse
-  // });
-  // let dataArray = data.split('\n').map((row) => row.split(','));
-  // let dataToInsert = dataArray.map(current => {
-  //   return {
-  //     question_id: Number(current[0]),
-  //     product_id: Number(current[1]),
-  //     question_body: current[2],
-  //     question_date: current[3],
-  //     asker_name: current[4],
-  //     asker_email: current[5],
-  //     reported: current[6],
-  //     question_helpfulness: Number(current[7])
-  //   };
-  // });
-  // console.log(dataToInsert);
-  // Question.create(dataToInsert)
-  // .then(() => mongoose.connection.close())
-  // .catch((err) => console.log(err));
+function addQuestions() {
+  let filename = path.join(__dirname, 'questions.csv')
+  let errors = [];
+  let counter = 0;
+  let batch = []
+  const stream = fs.createReadStream(filename)
+  .pipe(csv(['question_id', 'product_id', 'question_body', 'question_date', 'asker_name', 'asker_email', 'reported', 'question_helpfulness']))
+  .on('data', (data) => {
+      batch.push(data)
+      counter ++
+      if(counter > 1000) {
+        stream.pause()
+        Question.create(batch)
+        .then(data => {
+          counter = 0;
+          batch = []
+          stream.resume()
+        })
+        .catch(err => {
+          console.log("Error here!!!!\n", err);
+          counter = 0;
+          batch = []
+          stream.resume()
+        })
+      }
+  })
+  .on('error', (e) => {
+    console.error("What?\n", e)
+  })
+  .on('end', () => {
+    Question.create(batch)
+    .then(data => {
+      counter = 0;
+      batch = []
+    })
+    .catch(err => {
+      console.log("Error here!!!!\n", err);
+    })
+    console.log('end');
+  });
 };
 
-addPreviousData();
 
 
 
+addQuestions();
 
 
 module.exports = Question;
